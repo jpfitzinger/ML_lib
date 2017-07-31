be1636e0bc094ef18b79f0c79c464b78aa973ef1
jpfitzinger <jpfitzinger@xmlh.de>
2017-07-31 14:26:20 +0200

    Updated functions

--- method_lib.R
+++ method_lib.R
@@ -3,12 +3,24 @@
 # METHOD LIBRARY
 # ------------------------------------------------------------------------------------------
 
-      method_lib <- list(
-        decision_tree_r = list(name = "regr.rpart",
-                               params = list(
-                                 minsplit = list(name = "minsplit", type = "Integer", lower = 10, upper = 50, values = NULL),
-                                 minbucket = list(name = "minbucket", type = "Integer", lower = 10, upper = 50, values = NULL)
-                               )
-      )
-      )
-      
+method_lib <- list(
+  
+  # Decision Tree
+  rpart = list(name = "regr.rpart",
+               params = list(
+                 # Add Parameter Ranges below
+                 minsplit = list(name = "minsplit", type = "Integer", lower = 10, upper = 11, values = NULL),
+                 minbucket = list(name = "minbucket", type = "Integer", lower = 10, upper = 15, values = NULL)
+               )
+  ),
+  
+  # Conditional Tree
+  ctree = list(name = "regr.ctree",
+               params = list(
+                 # Add Parameter Ranges below
+                 minsplit = list(name = "minsplit", type = "Integer", lower = 1, upper = 5, values = NULL),
+                 minbucket = list(name = "minbucket", type = "Integer", lower = 10, upper = 50, values = NULL),
+                 mtry = list(name = "mtry", type = "Integer", lower = 0, upper = 10, values = NULL)
+               )
+  )
+)
